1e7a0a859fa9e76a75dea8cac5e587ececa792fd
Jesse-Zhang <sharpshui@hotmail.com>
2025-04-29 04:34:40 -0700

    update seed data & s3 image

--- client/next.config.ts
+++ client/next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "*.amazonaws.com",
+        port: "",
+        pathname: "/**"
+      }
     ],
   },
 };
